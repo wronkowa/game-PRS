d978462aa52e79769d2cb3104875bc4c2026df76
Magdalena Wrona <wrona_m@wp.pl>
2019-03-20 08:31:51 +0100

    add game action - player choose hand and ai random

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-// header button - rules
-document.querySelector('header button').addEventListener('click', () => {
-  document.querySelector(".rules").classList.add('active');
-  document.querySelector(".container").classList.add('active');
-})
+const handsHtml = [...document.querySelectorAll('.chooseHand i')];
 
-document.querySelector('.rules i').addEventListener('click', () => {
-  document.querySelector(".rules").classList.remove('active');
-  document.querySelector(".container").classList.remove('active');
-})
+const statistic = {
+  game: 0,
+  winner: 0,
+  looser: 0,
+  draw: 0,
+}
+let playerChoose = '';
+let aiChoose = '';
+
+const handChoose = (e) => {
+  playerChoose = e.target.dataset.option;
+  console.log(playerChoose);
+  handsHtml.forEach(hand => hand.classList.remove('active'));
+  e.target.classList.add('active');
+  document.querySelector('.game #playerChoose').textContent = playerChoose;
+}
+
+
+
+const startGame = () => {
+  // console.log('start');
+  if (playerChoose === '') alert('Wybierz dłoń!')
+  else {
+    // console.log('gramy');
+    aiChoose = handsHtml[Math.floor(Math.random() * 3)].dataset.option;
+    document.querySelector('.game #aiChoose').textContent = aiChoose;
+  }
+
+}
+
+
+//clik na dlonie
+handsHtml.forEach(hand => hand.addEventListener("click", handChoose))
+
+// przycisk graj:
+document.querySelector('.play button').addEventListener('click', startGame);

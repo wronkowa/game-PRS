22b0d002ca1228149024efbad45011a0af67086c
Magdalena Wrona <wrona_m@wp.pl>
2019-03-20 09:03:08 +0100

    add statistic and logic who win

--- main.js
+++ main.js
@@ -17,7 +17,28 @@ const handChoose = (e) => {
   document.querySelector('.game #playerChoose').textContent = playerChoose;
 }
 
-
+const whoWin = () => {
+  if (playerChoose === aiChoose) {
+    console.log('remis');
+    statistic.draw++;
+    document.querySelector('.statistic #draw').textContent = statistic.draw;
+    document.querySelector('.game #whoWin').textContent = 'REMIS';
+  }
+  else if ((playerChoose === 'papier' && aiChoose === 'kamień') || (playerChoose === 'kamień' && aiChoose === 'nożyczki') || (playerChoose === 'nożyczki' && aiChoose === 'papier')) {
+    console.log('wygrana');
+    statistic.winner++;
+    document.querySelector('.statistic #winner').textContent = statistic.winner;
+    document.querySelector('.game #whoWin').textContent = 'wygrałeś!';
+  }
+  else
+  //  if ((playerChoose === 'papier' && aiChoose === 'nożyczki') || (playerChoose === 'kamień' && aiChoose === 'papier'))
+  {
+    console.log('przegrana');
+    statistic.looser++;
+    document.querySelector('.statistic #looser').textContent = statistic.looser;
+    document.querySelector('.game #whoWin').textContent = 'Przegrałeś :(';
+  }
+}
 
 const startGame = () => {
   // console.log('start');
@@ -26,6 +47,10 @@ const startGame = () => {
     // console.log('gramy');
     aiChoose = handsHtml[Math.floor(Math.random() * 3)].dataset.option;
     document.querySelector('.game #aiChoose').textContent = aiChoose;
+    statistic.game++;
+    // console.log(statistic.game);
+    document.querySelector('.statistic #game').textContent = statistic.game;
+    whoWin();
   }
 
 }
